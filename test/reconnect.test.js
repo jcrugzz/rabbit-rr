6788978df4a09bb0542f6f20ab106c70f3cc892f
Jarrett Cruger <jcrugzz@gmail.com>
2015-06-04 23:45:29 -0400

    [test] modify stub

--- test/reconnect.test.js
+++ test/reconnect.test.js
@@ -2,19 +2,22 @@ var Rabbit = require('..');
 var exec = require('child_process')
 var assume = require('assume');
 
-describe('reconnect test case', function () {
-  var conn = new Rabbit();
-  var oConn = new Rabbit();
+describe.skip('reconnect test case', function () {
 
-  var req = conn.socket('REQ')
-    .connect('whatever_whatever_man')
-    .on('connect', function () {
-      console.log('req connect');
-    })
-    .send({ what: 'foo' }, function (err, msg) {
+  it('should properly reconnect when rabbit goes down', function (done) {
 
-    });
+    var conn = new Rabbit();
+    var oConn = new Rabbit();
 
-  var rep =
+    var req = conn.socket('REQ')
+      .connect('whatever_whatever_man')
+      .on('connect', function () {
+        console.log('req connect');
+      })
+      .send({ what: 'foo' }, function (err, msg) {
+
+      });
+
+  });
 
 });
